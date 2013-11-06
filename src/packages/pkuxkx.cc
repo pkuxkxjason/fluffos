49262471b4c39b6c60404f27e32c6c63b69aed28
pkuxkxjason <zhuce9981@sina.cn>
2013-11-06 22:02:09 +0800

    utf8 package

--- src/packages/pkuxkx.cc
+++ src/packages/pkuxkx.cc
@@ -15,20 +15,51 @@
 #include "../port.h"
 #include "../array.h"
 
+#include <iconv.h>
+
+
 #ifdef PACKAGE_PKUXKX
 
-char* translate_easy(char* encoding, char* input)
+char *translate(iconv_t tr, const char *input)
 {
-  char* output = (char *)DMALLOC(strlen(input) + 1, TAG_PERMANENT, "translate");	
-  strcpy(output,input);
-  output[0] = '+';
-  return output;
+  size_t inlen = strlen(input) + 1;  
+  char *output = 0, *output_pre;
+  size_t outlen = 0, outlen_pre;
+
+  if (!output) {
+    output_pre = output = (char *)DMALLOC(inlen, TAG_PERMANENT, "gbutf8");
+    outlen_pre = outlen = inlen;
+  }
+
+  while (inlen) {
+    iconv(tr, (char **)&input, &inlen, &output, &outlen);
+
+		if (E2BIG == errno) {
+			errno = 0;
+			outlen_pre *= 2;
+			output = (char *)DREALLOC(output_pre, outlen_pre, TAG_PERMANENT, "gbutf8");
+			if(!output) {
+				FREE(output_pre);
+				return 0;
+			}
+			output_pre = output;
+			output = output_pre + (outlen_pre / 2 - outlen);
+			outlen = outlen + outlen_pre / 2;
+			continue;
+		}
+	}
+  return output_pre;
 }
+
 
 void f_gb_to_utf8()
 {
   char *text = (char *)sp->u.string;
-  char *translated = translate_easy("gbk", text);
+
+  iconv_t t = iconv_open("UTF-8", "gbk");
+  char *translated = translate(t, text);
+	iconv_close(t);
+
   pop_stack();
   copy_and_push_string(translated);
 	
@@ -36,10 +67,13 @@ void f_gb_to_utf8()
 void f_utf8_to_gb()
 {
   char *text = (char *)sp->u.string;
-  char *translated = translate_easy("gbk", text);
+
+  iconv_t t = iconv_open("gbk", "UTF-8");
+  char *translated = translate(t, text);
+	iconv_close(t);
+
   pop_stack();
-  copy_and_push_string(translated);
-	
+  copy_and_push_string(translated);	
 }
 #endif
 
